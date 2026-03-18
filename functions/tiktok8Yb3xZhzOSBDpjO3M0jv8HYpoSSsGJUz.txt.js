96e9d4bbf689cbb0b8c7b0682c7c5e1ed395c9f5
Sally Matthes <sallymatthes@googlemail.com>
2026-03-18 16:40:31 +0000

    TikTok verification via CF Pages Function (build workaround)

---
+++ functions/tiktok8Yb3xZhzOSBDpjO3M0jv8HYpoSSsGJUz.txt.js
@@ -0,0 +1,5 @@
+export function onRequest() {
+  return new Response("tiktok-developers-site-verification=8Yb3xZhzOSBDpjO3M0jv8HYpoSSsGJUz\n", {
+    headers: { "Content-Type": "text/plain" }
+  });
+}
